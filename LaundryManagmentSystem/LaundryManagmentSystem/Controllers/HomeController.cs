9f0c33a8ba4449c9cc6f7e750b48cff9552b0aa1
AnkitaCoditas <ankita.kumari@coditas.com>
2022-11-21 05:34:00 -0800

    Login Done

---
+++ LaundryManagmentSystem/LaundryManagmentSystem/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using LaundryManagmentSystem.Models;
+namespace LaundryManagmentSystem.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index(User user)
+        {
+
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                
+                return View();  
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LaundryManagmentSystem/LaundryManagmentSystem/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LaundryManagmentSystem.Models;
+
+namespace LaundryManagmentSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        LaundryManagmentSystemEntitiess db=new LaundryManagmentSystemEntitiess();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+           
+        {
+            var result = db.Users.Where(model => model.UserName == user.UserName && model.Password == user.Password).FirstOrDefault();
+
+            if(result!=null)
+            {
+                Session["UserId"] = user.User_ID.ToString();
+                Session["UserName"]=user.UserName.ToString();
+                TempData["LoginMessage"]= "<script>alert('LOGIN SUCCESSFULL')</script>";
+                return RedirectToAction("Index", "Home");
+
+               
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('UserName OR Password Is Incorrect!')</script>";
+
+                return View();
+            }
+
+           
+        }
+
+
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.Users.Add(user); 
+                var result =db.SaveChanges();   
+                if(result>0)
+                {
+                    ViewBag.InsertMessage= "<script>alert('REGISTERED SUCCESSFULLY')</script>";
+                    ModelState.Clear();
+                    
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('REGISTration failed')</script>";
+
+                }
+            }
+            return View();
+        }
+
+    }
+}
